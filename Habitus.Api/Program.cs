9963b96b4ca3c99a1578132b66af2ba12a5a16bd
Hugo Santos <hugosantos0021@gmail.com>
2026-05-09 17:35:47 -0300

    PostgreSQL com Docker e Entity Framework configurados

--- Habitus.Api/Program.cs
+++ Habitus.Api/Program.cs
@@ -1,6 +1,12 @@
+using Habitus.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
